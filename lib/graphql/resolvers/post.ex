5a9aadc022971ef9ece87506a3345a29e25258bb
factsfinder <phanindrasrikar@gmail.com>
2020-09-29 07:57:17 +0530

    much better get posts query. also made like/dislike and create comment as a transaction

--- lib/graphql/resolvers/post.ex
+++ lib/graphql/resolvers/post.ex
@@ -23,56 +23,56 @@ defmodule API.Graphql.Resolvers.Post do
   end
 
   def updatePost(_, args, %{context: %{current_user: current_user}}) do
-    case Repo.get_by(Post, id: args.id, creator_id: current_user.id) do
-      %Post{} ->
-        case Post.changeset(%Post{}, args).change() |> Repo.update() do
-          {:ok, post} -> {:ok, post}
-          {:error, _} -> {:error, "error updating the post"}
-        end
+    post = Repo.get_by(Post, id: args.id, creator_id: current_user.id)
+
+    case Post.changeset(post, args).change() |> Repo.update() do
+      {:ok, res} -> {:ok, res}
+      {:error, _} -> {:error, "error updating the post"}
     end
   end
 
   def togglePostLike(_, args, %{context: %{current_user: current_user}}) do
     args_with_creator_id = Map.merge(args, %{creator_id: current_user.id})
+    Repo.transaction(fn ->
+      case Repo.get_by(PostLike, creator_id: current_user.id, post_id: args.post_id) do
+        nil ->
+          case Repo.insert(PostLike.changeset(%PostLike{}, args_with_creator_id)) do
+            {:ok, _} ->
+              case updateLikesCount(args.post_id, true) do
+                {:ok, _} -> true
+                {:error, _} ->
+                  Repo.rollback("error updating post like -> rolling back.")
+              end
 
-    case Repo.get_by(PostLike, creator_id: current_user.id, post_id: args.post_id) do
-      nil ->
-        case Repo.insert(PostLike.changeset(%PostLike{}, args_with_creator_id)) do
-          {:ok, _} ->
-            case updateLikesCount(args.post_id, true) do
-              {:ok, _} -> {:ok, true}
-              {:error, _} -> {:error, "error liking the post"}
-            end
+            {:error, _} ->
+              Repo.rollback("error updating post like -> rolling back.")
+          end
 
-          {:error, _} ->
-            {:error, "error liking the post"}
-        end
+        post_like ->
+          case Repo.delete(post_like) do
+            {:ok, _} ->
+              case updateLikesCount(args.post_id, false) do
+                {:ok, _} -> true
+                {:error, _} ->
+                  Repo.rollback("error updating post like -> rolling back.")
+              end
 
-      post_like ->
-        case Repo.delete(post_like) do
-          {:ok, _} ->
-            case updateLikesCount(args.post_id, false) do
-              {:ok, _} -> {:ok, true}
-              {:error, _} -> {:error, "error unliking the post"}
-            end
+            {:error, _} ->
+              Repo.rollback("error updating post like -> rolling back.")
+          end
+      end
+    end)
 
-          {:error, _} ->
-            {:error, "error unliking the post"}
-        end
-    end
   end
 
   defp updateLikesCount(post_id, isInc) do
     post = Repo.get(Post, post_id)
-
     case post do
       nil ->
-        {:error, "error updating post"}
-
+        {:error, "error updating post likes count"}
       post ->
         new_likes_count = if isInc, do: post.likes_count + 1, else: post.likes_count - 1
         updated = Repo.update(change(post, likes_count: new_likes_count))
-
         case updated do
           {:ok, _} -> {:ok, true}
           {:error, _} -> {:error, "error updating the post likes count"}
@@ -82,32 +82,55 @@ defmodule API.Graphql.Resolvers.Post do
 
   def createComment(_, args, %{context: %{current_user: current_user}}) do
     args_with_creator_id = Map.merge(args, %{creator_id: current_user.id})
+    Repo.transaction(fn ->
+      case PostComment.changeset(%PostComment{}, args_with_creator_id) |> Repo.insert() do
+        {:ok, new_comment} ->
+          case updateCommentsCount(args.post_id, true) do
+            {:ok, _} ->  Map.merge(new_comment, %{creator: current_user})
+            {:error, _} ->  Repo.rollback("error updating comments count so rolling back created comment...!")
+          end
+        {:error, _} ->
+          Repo.rollback("error creating a new comment -> rolling back.")
+      end
+    end)
+  end
 
-    case PostComment.changeset(%PostComment{}, args_with_creator_id) |> Repo.insert() do
-      {:ok, new_comment} ->
-        {:ok,
-         Map.merge(new_comment, %{
-           creator: current_user
-         })}
+  # Note: isInc will be false when we are deleting a comment
+  defp updateCommentsCount(post_id, isInc) do
+    post = Repo.get(Post, post_id)
+    case post do
+      nil ->
+        {:error, "error updating post comments count"}
+      _ ->
+        new_comments_count = if isInc, do: post.comments_count + 1, else: post.comments_count - 1
+        updated = Repo.update(change(post, comments_count: new_comments_count))
 
-      {:error, _} ->
-        {:error, "error creating a new comment"}
+        case updated do
+          {:ok, _} -> {:ok, true}
+          {:error, _} -> {:error, "error updating the post comments count"}
+        end
     end
   end
+
 
   def getPosts(_, %{page: page, type: type}, %{
         context: %{current_user: current_user}
       }) do
+
     query =
       from(post in Post,
         join: post_creator in assoc(post, :user),
+        left_join: likes in assoc(post, :post_likes),
         left_join: comments in assoc(post, :post_comments),
         left_join: comment_creator in assoc(comments, :user),
         preload: [user: post_creator, post_comments: {comments, user: comment_creator}],
         where: post.posted_to == ^type and is_nil(post.archived_at),
         order_by: [desc: post.inserted_at],
         offset: 10 * (^page - 1),
-        limit: 10
+        limit: 10,
+        select_merge: %{
+          liked_by_me: likes.post_id == post.id and likes.creator_id == ^current_user.id
+        }
       )
 
     posts =
@@ -132,13 +155,8 @@ defmodule API.Graphql.Resolvers.Post do
           end)
 
         Map.merge(post, %{
-          liked_by_me: post.creator_id == current_user.id,
           creator: post_creator,
-          recent_comments: recent_comments,
-          likes_count:
-            Repo.one(from(s in PostLike, where: s.post_id == ^post.id, select: count(s.id))),
-          comments_count:
-            Repo.one(from(c in PostComment, where: c.post_id == ^post.id, select: count(c.id)))
+          recent_comments: recent_comments
         })
       end)
 
